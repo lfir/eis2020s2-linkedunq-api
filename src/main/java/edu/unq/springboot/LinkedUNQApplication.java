ed8e2486f3ad47f773af91485841e6113a051337
Asta86 <psljp@protonmail.com>
2020-10-13 09:13:06 -0300

    Initial commit

---
+++ src/main/java/edu/unq/springboot/LinkedUNQApplication.java
@@ -0,0 +1,13 @@
+package edu.unq.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedUNQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedUNQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/unq/springboot/LinkedUNQAPIApplication.java
@@ -0,0 +1,13 @@
+package edu.unq.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedUNQAPIApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
