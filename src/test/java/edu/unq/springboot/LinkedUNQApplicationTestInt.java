179f4632fb2c8e06d938f24c207ef37cb656ed0b
Asta86 <psljp@protonmail.com>
2020-10-20 16:06:14 -0300

    Rename LinkedUNQApplicationTest

--- src/test/java/edu/unq/springboot/LinkedUNQApplicationTestInt.java
+++ src/test/java/edu/unq/springboot/LinkedUNQApplicationTestInt.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LinkedUNQApplicationTest {
+class LinkedUNQApplicationTestInt {
 
 	@Test
 	void contextLoads() {
