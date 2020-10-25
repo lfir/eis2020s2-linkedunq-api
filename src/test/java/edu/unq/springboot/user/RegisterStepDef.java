5cc22c1284993690415fee043d40a7ca78584bee
Mauricio Bianchi <mobianchi@cloodie.com>
2020-10-25 20:53:58 -0300

    fix 404 error

--- src/test/java/edu/unq/springboot/user/RegisterStepDef.java
+++ src/test/java/edu/unq/springboot/user/RegisterStepDef.java
@@ -1,37 +1,31 @@
 package edu.unq.springboot.user;
 
 
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
 import edu.unq.springboot.controllers.AccountController;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@ContextConfiguration(classes = TestConfiguration.class)
 @CucumberContextConfiguration
-
+@WebMvcTest(AccountController.class)
 public class RegisterStepDef {
     @Autowired
     private MockMvc mvc;
@@ -44,14 +38,12 @@ public class RegisterStepDef {
         mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         MockUser user = new MockUser("Jose", "Sand");
         String json = mapper.writeValueAsString(user);
-        action=mvc.perform(MockMvcRequestBuilders.post("/register")
-                .contextPath("/register")
-                .contentType(MediaType.APPLICATION_JSON));
+
     }
 
     @Then("Response status code of 200")
     public void response_ok() throws Exception {
-        action.andExpect(status().isOk());
+        mvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
-
 }
