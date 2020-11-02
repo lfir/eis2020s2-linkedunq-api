6d0f38ed30b5db6555b30af45c739431ce796816
Asta86 <psljp@protonmail.com>
2020-11-02 11:19:57 -0300

    Add cucumber test findjobs.feature

--- src/test/java/edu/unq/springboot/cucumber/userSteps/CreateJobStepDef.java
+++ src/test/java/edu/unq/springboot/cucumber/userSteps/CreateJobStepDef.java
@@ -1,8 +1,15 @@
 package edu.unq.springboot.cucumber.userSteps;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +24,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import edu.unq.springboot.models.CreateJobRequestBody;
+import edu.unq.springboot.models.Job;
+import edu.unq.springboot.models.User;
 import edu.unq.springboot.service.JobService;
 import edu.unq.springboot.service.UserService;
 import io.cucumber.java.en.Then;
@@ -29,9 +38,9 @@ import io.cucumber.spring.CucumberContextConfiguration;
 public class CreateJobStepDef {
     @Autowired
     private MockMvc mvc;
-    @MockBean
+    @Autowired @MockBean
     private UserService userService;
-    @MockBean
+    @Autowired @MockBean
     private JobService jobService;
     private ResultActions action;
 
@@ -42,6 +51,24 @@ public class CreateJobStepDef {
         CreateJobRequestBody bd = new CreateJobRequestBody("Jose123", "titulo", "desc", "2010-01-01", "2010-01-01");
         String json = mapper.writeValueAsString(bd);
         action = mvc.perform(post("/jobs/create").content(json).contentType(MediaType.APPLICATION_JSON));
+    }
+    
+    @When("Request to get jobs of user 'user0'")
+    public void request_to_get_jobs_of_user0() throws Exception {
+    	User usuarioDos = new User("DosSantos", "pass", "fname", "lname", "correo");
+		Job trabajo = new Job(usuarioDos, "Titulo", "Descripcion", LocalDate.parse("2010-10-20"), LocalDate.parse("2015-08-10"));
+    	List<Job> jobDataAsList = new ArrayList<Job>();
+    	jobDataAsList.add(trabajo);
+    	given(jobService.findByUsername(usuarioDos.getUsername())).willReturn(jobDataAsList);
+
+    	action = mvc.perform(get("/jobs").param("username", usuarioDos.getUsername()));
+    }
+    
+    @Then("List of 'user0' jobs is received")
+    public void get_jobs_response_ok() throws Exception {
+        action.andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(content().string(containsString("Descripcion")));
     }
     
     @Then("The new job is created and saved")
